9b21d67873947478bce08326693a9801c899effd
Kevin Buhler <buhlerkw@gmail.com>
2023-12-12 23:19:29 -0700

    small update

--- unfollow.py
+++ unfollow.py
@@ -2,8 +2,8 @@ import json
 import zipfile
 
 # put your zip file name here 
-# for example: dir_name = 'kevinbuhlerr_20230215.zip'
-zip_file_path = 'kevinbuhlerr_20230328.zip'
+# for example: dir_name = 'instagram-username-date-random.zip'
+zip_file_path = 'instagram-kevinbuhlerr-2023-12-12-uYbuX9Yq.zip'
 
 with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
     zip_ref.extractall('data')
@@ -18,17 +18,15 @@ def parse_following(json_file):
     curr_set = set()
     for user in json_file['relationships_following']:
       curr_set.add(user['string_list_data'][0]['value'])
-      # pass
-      # print(user['string_list_data'][0]['value'])
+      print("following: ", user['string_list_data'][0]['value'])
     return curr_set
 
 def parse_followers(json_file):
     json_file = json_file
     curr_set = set()
     for user in json_file:
-      # print
-      # print(user['string_list_data'][0])
       curr_set.add(user['string_list_data'][0]['value'])
+      print("follower: ",user['string_list_data'][0])
     return curr_set
 
 following_set = parse_following(following_json)
@@ -44,7 +42,6 @@ print("")
 
 print(f"Not Following:")
 print(f"----------------------------------------")
-
 
 for i,user_not_following in enumerate(not_following):
   print(f"{i+1}.", user_not_following)
